b6061ba9fd52d85b74c439cda50f42ab8f5cb527
srirag-vuppala <vuppala@calpoly.edu>
2021-02-14 23:14:19 -0800

    Added navbar along with some cleanup

---
+++ react_frontend/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../assets/NavBar.css'
+import { Link } from 'react-router-dom'
+
+
+function NavBar() {
+    return (
+        <>
+            <nav className = "nav-bar">
+                {/* <img className="logo" src= {imager} alt="temp-logo" />  */}
+                <ul className="nav-links">
+                    <li className="title"><Link to ="/">Internship Logger</Link></li>
+                    <li className="spreadsheet-li"><Link to ="/Spreadsheet">Spreadsheet</Link></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar

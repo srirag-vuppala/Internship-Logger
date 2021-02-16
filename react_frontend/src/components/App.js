3919fd154a4f367bc0f0eee1992f6edd046dce46
hhokari <hhokari@calpoly.edu>
2021-02-16 01:28:00 -0800

    Updated font weight for search button

--- react_frontend/src/components/App.js
+++ react_frontend/src/components/App.js
@@ -80,8 +80,8 @@ class App extends Component {
         {/*<Table characterData={characters} removeCharacter={this.removeCharacter} />
         <Form handleSubmit={this.handleSubmit}/>*/}
         {/*<SearchBar searchBoxName={"userNameSearch"} onSearchTermChange={this.onSearch} />*/}
-        <input type="text"  style={{placeholderTextColor: '#000000',fontFamily:'Montserrat', textAlign: 'center', position: 'absolute', width: 587.53, height: 77, left: 504.77, top: 256, background: '#D3D3D3', fontSize: 36}} placeholder="Search Bar"/>
-        <input type="button" value="Search" style={{color: '#FFFFFF', fontFamily:'Montserrat', background: '#660708', position: 'absolute', width: 174, height: 69.32, left: 1146, top: 256.16, borderRadius: 33.5, fontSize: 24}} onClick={this.submitForm} />
+        <input type="text" placeholder="Search Bar" placeholderTextColor="#000000" style={{fontFamily:'Montserrat', textAlign: 'center', position: 'absolute', width: 587.53, height: 77, left: 504.77, top: 256, background: '#D3D3D3', fontSize: 36}} />
+        <input type="button" value="Search" style={{color: '#FFFFFF', fontFamily:'Montserrat', background: '#660708', position: 'absolute', width: 174, height: 69.32, left: 1146, top: 256.16, borderRadius: 33.5, fontSize: 24, fontWeight: 'bold'}} onClick={this.submitForm} />
       </div>
     )
   }

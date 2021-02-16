737a8ba8e23ed4c13dded4dccdae164a304cfd8c
hhokari <hhokari@calpoly.edu>
2021-02-16 00:06:25 -0800

    Fixed "most" of the styling for Search Bar and Button

--- react_frontend/src/components/App.js
+++ react_frontend/src/components/App.js
@@ -77,11 +77,11 @@ class App extends Component {
     const { characters } = this.state
     return(
       <div className="container">
-        <Table characterData={characters} removeCharacter={this.removeCharacter} />
-        <Form handleSubmit={this.handleSubmit}/>
+        {/*<Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit}/>*/}
         {/*<SearchBar searchBoxName={"userNameSearch"} onSearchTermChange={this.onSearch} />*/}
-        <input type="text"  style={{textAlign: 'center'}} placeholder="Search..." />
-        <input type="button" value="Search" style={{color: '#FFFFFF', background: '#660708'}} onClick={this.submitForm} />
+        <input type="text"  style={{placeholderTextColor: '#000000',textAlign: 'center', position: 'absolute', width: 587.53, height: 77, left: 504.77, top: 256, background: '#D3D3D3', fontSize: 36}} placeholder="Search Bar"/>
+        <input type="button" value="Search" style={{color: '#FFFFFF', background: '#660708', position: 'absolute', width: 174, height: 69.32, left: 1146, top: 256.16, borderRadius: 33.5, fontSize: 24}} onClick={this.submitForm} />
       </div>
     )
   }

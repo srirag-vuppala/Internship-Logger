e0fe7923488b6004d446d0fad1071bc4b200e0f1
srirag-vuppala <vuppala@calpoly.edu>
2021-02-22 13:45:24 -0800

    fixed the app.js and put test in home.js

--- react_frontend/src/components/App.js
+++ react_frontend/src/components/App.js
@@ -1,93 +1,102 @@
 import React, { Component } from 'react'
-import Table from './Table'
-import Form from './Form'
-import axios from 'axios'
-import SearchBar from './SearchBar'
-import SearchButton from './SearchButton'
+import NavBar from './NavBar'
+import Spreadsheet from './Spreadsheet'
+import Home from './Home'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 
 
 class App extends Component {
-/*   state = {
-    characters: [],
-  }
-
-  removeCharacter = index => {
-    const { characters } = this.state
-
-    //this.makeDeleteCall(characters[index])
-    axios.delete('http://localhost:5000/users', {data : characters[index]})
-
-
-    this.setState({ 
-      characters: characters.filter((character, i) => {
-        return i !== index
-      }),
-    })
-  }
-
-  makeDeleteCall(object){
-    return axios.delete('http://localhost:5000/users', {
-      data : {
-        'id': object.id,
-        'name': object.name,
-        'job': object.job
-    }})
-    .then(function(response) {
-      console.log(response);
-      return (response.status === 200);
-    })
-    .catch(function (error){
-      console.log(error);
-      return false;
-    });
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:5000/users')
-    .then(res => {
-      const characters = res.data.users_list;
-      this.setState({ characters })
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-
-  makePostCall(character){
-    return axios.post('http://localhost:5000/users', character)
-    .then(function(response) {
-      console.log(response);
-      return response
-    })
-    .catch(function (error){
-      console.log(error);
-      return false;
-    });
-  }
-
-  handleSubmit = character => {
-    this.makePostCall(character).then( callResult => {
-      character.id = callResult.id
-      if (callResult.status === 201) {
-        this.setState({ characters: [...this.state.characters, callResult.data] });
-      }
-    });
-  } */
-
   render() {
-    const { characters } = this.state
+    // const { characters } = this.state
     return(
-      <div className="container">
-        {/*<Table characterData={characters} removeCharacter={this.removeCharacter} />
-        <Form handleSubmit={this.handleSubmit}/>*/}
-        {/*<SearchBar searchBoxName={"userNameSearch"} onSearchTermChange={this.onSearch} />*/}
-        {/*<input type="text" placeholder="Search Bar" placeholderTextColor="#000000" style={{fontFamily:'Montserrat', textAlign: 'center', position: 'absolute', width: 587.53, height: 77, left: 504.77, top: 256, background: '#D3D3D3', fontSize: 36}} />*/}
-        <SearchBar />
-        <SearchButton />
-        {/*<input type="button" value="Search" style={{color: '#FFFFFF', fontFamily:'Montserrat', background: '#660708', position: 'absolute', width: 174, height: 69.32, left: 1146, top: 256.16, borderRadius: 33.5, fontSize: 24, fontWeight: 'bold'}} onClick={this.submitForm} />*/}
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Switch>
+            <Route exact path="/Spreadsheet" component={Spreadsheet}/> 
+            <Route exact path="/" component={Home}/> 
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
+
+//   state = {
+//     characters: [],
+//   }
+//   removeCharacter = index => {
+//     const { characters } = this.state
+
+//     //this.makeDeleteCall(characters[index])
+//     axios.delete('http://localhost:5000/users', {data : characters[index]})
+
+
+//     this.setState({ 
+//       characters: characters.filter((character, i) => {
+//         return i !== index
+//       }),
+//     })
+//   }
+
+//   makeDeleteCall(object){
+//     return axios.delete('http://localhost:5000/users', {
+//       data : {
+//         'id': object.id,
+//         'name': object.name,
+//         'job': object.job
+//     }})
+//     .then(function(response) {
+//       console.log(response);
+//       return (response.status === 200);
+//     })
+//     .catch(function (error){
+//       console.log(error);
+//       return false;
+//     });
+//   }
+
+//   componentDidMount() {
+//     axios.get('http://localhost:5000/users')
+//     .then(res => {
+//       const characters = res.data.users_list;
+//       this.setState({ characters })
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+//   }
+
+//   makePostCall(character){
+//     return axios.post('http://localhost:5000/users', character)
+//     .then(function(response) {
+//       console.log(response);
+//       return response
+//     })
+//     .catch(function (error){
+//       console.log(error);
+//       return false;
+//     });
+//   }
+
+//   handleSubmit = character => {
+//     this.makePostCall(character).then( callResult => {
+//       character.id = callResult.id
+//       if (callResult.status === 201) {
+//         this.setState({ characters: [...this.state.characters, callResult.data] });
+//       }
+//     });
+//   }
+
+//   render() {
+//     const { characters } = this.state
+//     return(
+//       <div className="container">
+//         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+//         <Form handleSubmit={this.handleSubmit}/>
+//       </div>
+//     )
+//   }
+// }
 
 export default App

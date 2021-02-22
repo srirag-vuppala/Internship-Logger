f53e04e39c356b7943c661c809fe321049c6ff4a
hhokari <hhokari@calpoly.edu>
2021-02-22 12:22:53 -0800

    Calling Search Button function in App.js

--- react_frontend/src/components/App.js
+++ react_frontend/src/components/App.js
@@ -3,6 +3,7 @@ import Table from './Table'
 import Form from './Form'
 import axios from 'axios'
 import SearchBar from './SearchBar'
+import SearchButton from './SearchButton'
 
 
 class App extends Component {
@@ -82,7 +83,8 @@ class App extends Component {
         {/*<SearchBar searchBoxName={"userNameSearch"} onSearchTermChange={this.onSearch} />*/}
         {/*<input type="text" placeholder="Search Bar" placeholderTextColor="#000000" style={{fontFamily:'Montserrat', textAlign: 'center', position: 'absolute', width: 587.53, height: 77, left: 504.77, top: 256, background: '#D3D3D3', fontSize: 36}} />*/}
         <SearchBar />
-        <input type="button" value="Search" style={{color: '#FFFFFF', fontFamily:'Montserrat', background: '#660708', position: 'absolute', width: 174, height: 69.32, left: 1146, top: 256.16, borderRadius: 33.5, fontSize: 24, fontWeight: 'bold'}} onClick={this.submitForm} />
+        <SearchButton />
+        {/*<input type="button" value="Search" style={{color: '#FFFFFF', fontFamily:'Montserrat', background: '#660708', position: 'absolute', width: 174, height: 69.32, left: 1146, top: 256.16, borderRadius: 33.5, fontSize: 24, fontWeight: 'bold'}} onClick={this.submitForm} />*/}
       </div>
     )
   }

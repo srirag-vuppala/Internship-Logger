70dc1e43b63a504516f443009e6ef6d1053ab08a
hhokari <hhokari@calpoly.edu>
2021-02-22 12:21:10 -0800

    Refactored Search Button into own js file

---
+++ react_frontend/src/components/SearchButton.js
@@ -0,0 +1,12 @@
+import '../assets/SearchButton.css'
+
+function SearchButton() {
+    return (
+        <div className="search-button">
+            <input
+            type="button"
+            value="Search"
+            />
+        </div>
+    )
+}

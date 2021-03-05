06544ad47b23e56a2d305b98680399b7f3236f13
rsmith92 <63178284+rsmith92@users.noreply.github.com>
2021-03-04 18:31:19 -0800

    Update app.py

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -32,7 +32,7 @@ jobs = { # users before
       }
  ]
 }
-
+// macs function
 def getJobsFromQuery():
    search_status = request.args.get('status')
    search_name = request.args.get('company')
@@ -41,7 +41,7 @@ def getJobsFromQuery():
    elif search_name:
       jobs = Job().find_by_company(search_name)
    return {"job_list": jobs}                    # make sure this is returning correctly
-
+// right here
 
 # def editJob(): # can either do a delete and then an add or write a new mongo function that edits the job
 

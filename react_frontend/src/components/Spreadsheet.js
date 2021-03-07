b32168a6edc547fde46a2969548e77b038a0de40
hhokari <hhokari@calpoly.edu>
2021-03-06 20:14:52 -0800

    Deleted default text and replaced with 3 boxes for the position, company name, and additional info

--- react_frontend/src/components/Spreadsheet.js
+++ react_frontend/src/components/Spreadsheet.js
@@ -60,9 +60,27 @@ class Spreadsheet extends React.Component {
     />
     {this.state.isOpen && <Popup
       content={<>
-        <b>Design your Popup</b>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>Test button</button>
+        {/*<b>Design your Popup</b>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>*/}
+        <div className="Enter-Position-box">
+			<input
+			type="text"
+			placeholder="Enter Position"
+			/>
+		</div>
+		<div className="Enter-Company-Name-box">
+			<input
+			type="text"
+			placeholder="Enter Company Name"
+			/>
+		</div>
+		<div className="Additional-Information-box">
+			<input
+			type="text"
+			placeholder="Additional Information"
+			/>
+		</div>
+		<button>Test button</button>
       </>}
       handleClose={togglePopup}
     />}

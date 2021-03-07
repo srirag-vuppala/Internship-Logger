31bc85708e0982fb78fff81090c14df27db5320c
hhokari <hhokari@calpoly.edu>
2021-03-06 19:05:22 -0800

    Added popup to spreadsheet page, needs styling

--- react_frontend/src/components/Spreadsheet.js
+++ react_frontend/src/components/Spreadsheet.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from 'react-bootstrap/Card'
+import Popup from './Popup'
 
 
 const emoji = require("emoji-dictionary")
@@ -29,16 +30,44 @@ const emoji_choose = e => {
  * The Spreadsheet React component
  */
 class Spreadsheet extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isOpen: false
+		};
+	}
 	render(){
 		const style = {
 			'padding': '30px',
 			'paddingTop': '10px',
 			'font-family': 'Montserrat'
 		};
+		{/*const [isOpen, setIsOpen] = useState(false);*/}
+ 
+		const togglePopup = () => {
+		  this.setState({isOpen: !this.state.isOpen});
+		}
     
 		return(
+			
       <div style={ style }>
 			<SpreadsheetBoard />
+			<div>
+    <input
+      type="button"
+      value="Click to Open Popup"
+      onClick={togglePopup}
+    />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    {this.state.isOpen && <Popup
+      content={<>
+        <b>Design your Popup</b>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button>Test button</button>
+      </>}
+      handleClose={togglePopup}
+    />}
+  </div>
       </div>
 		);
 	}

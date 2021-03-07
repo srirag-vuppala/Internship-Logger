4842160922d14a7eb0bc0e09254a31d3c83012e4
hhokari <hhokari@calpoly.edu>
2021-03-06 21:27:22 -0800

    Put Current stage of process in more accurate position

--- react_frontend/src/components/Spreadsheet.js
+++ react_frontend/src/components/Spreadsheet.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
+import '../assets/Spreadsheet.css'
 import Card from 'react-bootstrap/Card'
 import Popup from './Popup'
 import Dropdown from './Dropdown_Button'
-import '../assets/Spreadsheet.css'
 
 
 const emoji = require("emoji-dictionary")
@@ -60,7 +60,7 @@ class Spreadsheet extends React.Component {
       value="Click to Open Popup"
       onClick={togglePopup}
     />
-    {this.state.isOpen && <Popup
+    {this.state.isOpen && <Popup className="add-popup"
       content={<>
         {/*<b>Design your Popup</b>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>*/}

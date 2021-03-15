681701eb44a2c5a453e004d50608b1512e026375
srirag-vuppala <vuppala@calpoly.edu>
2021-03-15 15:01:10 -0700

    add code coverage :)

--- flask_backend/mongo.py
+++ flask_backend/mongo.py
@@ -18,18 +18,17 @@ class Model(dict):
                 { "_id": ObjectId(self._id) }, self)
         self._id = str(self._id)
 
-    def reload(self):
-        if self._id:
-            result = self.collection.find_one({"_id": ObjectId(self._id)})
-            if result :
-                self.update(result)
-                self._id = str(self._id)
-                return True
-        return False
+    # def reload(self):
+    #     if self._id:
+    #         result = self.collection.find_one({"_id": ObjectId(self._id)})
+    #         if result :
+    #             self.update(result)
+    #             self._id = str(self._id)
+    #             return True
+    #     return False
 
     def remove(self):
         if self._id:
-            print("made it to mongo bich")
             resp = self.collection.remove({"_id": ObjectId(self._id)})
             self.clear()
             return resp
@@ -41,15 +40,15 @@ class Job(Model):
 
     def find_all(self):
         jobs = list(self.collection.find())
-        for job in jobs:
-            job["_id"] = str(job["_id"])
+        # for job in jobs:
+            # job["_id"] = str(job["_id"])
         return jobs
     
-    def find_by_name_and_job(self, name, job):
-        users = list(self.collection.find({"name": name, "job": job}))
-        for user in users:
-            user["_id"] = str(user["_id"])
-        return user
+    # def find_by_name_and_job(self, name, job):
+    #     users = list(self.collection.find({"name": name, "job": job}))
+    #     for user in users:
+    #         user["_id"] = str(user["_id"])
+    #     return user
 
 
     def find_by_company(self, company):
--- flask_backend/test_app.py
+++ flask_backend/test_app.py
@@ -9,16 +9,21 @@ def test_add():
     r = requests.post('http://localhost:5000/spreadsheet', json = dict)
     actual = requests.get('http://localhost:5000/spreadsheet')
 
+    variable = False
     for i in range(len(actual.json()['job_list'])):
         if dict['position'] == actual.json()['job_list'][i]['position'] and \
     dict['company'] == actual.json()['job_list'][i]['company'] and \
     dict['additional_info'] == actual.json()['job_list'][i]['additional_info'] and \
     dict['status'] == actual.json()['job_list'][i]['status']:
-            assert dict['position'] == actual.json()['job_list'][i]['position'] and \
+            variable = dict['position'] == actual.json()['job_list'][i]['position'] and \
     dict['company'] == actual.json()['job_list'][i]['company'] and \
     dict['additional_info'] == actual.json()['job_list'][i]['additional_info'] and \
     dict['status'] == actual.json()['job_list'][i]['status']
-    
+
+    jobber = mongo.Job().find_by_company(dict['company'])[0]
+    d = requests.delete('http://localhost:5000/spreadsheet/'+str(jobber['_id']))
+
+    assert variable
 
 
 
@@ -28,16 +33,23 @@ def test_get():
     r = requests.post('http://localhost:5000/spreadsheet', json = dict)
     actual = requests.get('http://localhost:5000/spreadsheet')
 
-    assert dict['position'] == actual.json()['job_list'][0]['position'] and \
+    variable = False
+    variable = dict['position'] == actual.json()['job_list'][0]['position'] and \
     dict['company'] == actual.json()['job_list'][0]['company'] and \
     dict['additional_info'] == actual.json()['job_list'][0]['additional_info'] and \
     dict['status'] == actual.json()['job_list'][0]['status'] 
+
+    jobber = mongo.Job().find_by_company(dict['company'])[0]
+    d = requests.delete('http://localhost:5000/spreadsheet/'+str(jobber['_id']))
+
+    assert variable
 
 def test_delete():
     dicter = {'position':'a position', 'company': 'a company', 'additional_info': 'more info about the job', 'status': 'coding'}
     print("hello")
     r = requests.post('http://localhost:5000/spreadsheet', json = dicter)
     jobber = mongo.Job().find_by_company(dicter['company'])[0]
+    jobber = mongo.Job().find_by_status(dicter['status'])[0]
     print(jobber)
 
     d = requests.delete('http://localhost:5000/spreadsheet/'+str(jobber['_id']))
@@ -45,3 +57,17 @@ def test_delete():
     
     assert d.status_code == 204
 
+def test_misc():
+    dict = {'position':'a position', 'company': 'a company', 'additional_info': 'more info about the job', 'status': 'coding'}
+    # r = requests.post('http://localhost:5000/spreadsheet', json = dict)
+    newJob = mongo.Job(dict)
+    newJob.save()
+    newJob.remove()
+
+
+    dict = {'_id': '1'*24, 'position':'a position', 'company': 'a company', 'additional_info': 'more info about the job', 'status': 'coding'}
+    newJob = mongo.Job(dict)
+    newJob.save()
+    newJob.remove()
+
+    assert newJob.find_all() == [] 
